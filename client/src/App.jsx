9ab64bdbb590cc1aa7b84b9d6b910c9b6f9bb537
ArcaneCipher <desjardins.jarp@gmail.com>
2025-04-04 01:14:18 -0400

    refactor: migrate global and component styles from CSS to modular SASS structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import api from "./api/axios";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
