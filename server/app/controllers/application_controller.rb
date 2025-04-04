a0a6ce4d114588251b1dde6b59e3cd73d7db3926
ArcaneCipher <desjardins.jarp@gmail.com>
2025-04-04 00:23:12 -0400

    Cleaned up ping route, extracted ExampleController, and simplified React client setup

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::API
-  # app/controllers/api/v1/example_controller.rb
-  module Api
-    module V1
-      class ExampleController < ApplicationController
-        def index
-          @items = Item.all
-          render json: @items
-        end
-      end
-    end
+  def ping
+    render json: {message: "pong from Rails"}
   end
 end
